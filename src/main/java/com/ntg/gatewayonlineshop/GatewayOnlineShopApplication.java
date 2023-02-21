1ed3afee1ecf4a3be82235d052d2f2477d2174cf
Nurbolot <ngulamidinov@gmail.com>
2023-02-21 18:03:19 +0600

    init commit

---
+++ src/main/java/com/ntg/gatewayonlineshop/GatewayOnlineShopApplication.java
@@ -0,0 +1,15 @@
+package com.ntg.gatewayonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayOnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayOnlineShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ntg/gatewayonlineshop/GatewayOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntg.gatewayonlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayOnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
